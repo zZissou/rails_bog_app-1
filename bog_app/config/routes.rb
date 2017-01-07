4b0850eaad1f11593c97450cc3b04260d2219cd4
zZissou <kyle.edward.russell@gmail.com>
2017-01-07 14:47:13 -0800

    new, create, and show are working

---
+++ bog_app/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'creatures#index'
+
+  get "/creatures", to: 'creatures#index', as: "creatures"
+  get "/creatures/new", to: 'creatures#new', as: "new_creature"
+  post "/creatures", to: "creatures#create"
+  get "/creatures/:id", to: "creatures#show", as: "creature"
+end
