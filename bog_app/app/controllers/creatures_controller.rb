b8977bd197f901c6897a19234499efb9179a2966
zZissou <kyle.edward.russell@gmail.com>
2017-01-07 15:19:20 -0800

    change a creature with edit and update

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -5,8 +5,6 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
-
     creature = Creature.new(creature_params)
 
     if creature.save
@@ -14,13 +12,35 @@ class CreaturesController < ApplicationController
     end
   end
 
-    def new
-      @creature = Creature.new
-    end
+  def new
+    @creature = Creature.new
+  end
 
-    def show
-      creature_id = params[:id]
+  def show
+    creature_id = params[:id]
 
-      @creature = Creature.find_by_id(creature_id)
-    end
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def edit
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature.update_attributes(creature_params)
+
+    redirect_to creature_path(creature)
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
 end
