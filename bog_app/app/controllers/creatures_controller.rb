96fbc6abf6323d7f63b4e494163c47e710d10e31
zZissou <kyle.edward.russell@gmail.com>
2017-01-07 15:25:40 -0800

    added destroy. finished bog app. time = 01h 02m 31s

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -38,6 +38,16 @@ class CreaturesController < ApplicationController
     redirect_to creature_path(creature)
   end
 
+  def destroy
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature.destroy
+
+    redirect_to creatures_path
+  end
+
   private
 
   def creature_params
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "/creatures/:id", to: "creatures#show", as: "creature"
   get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
   patch "/creatures/:id", to: "creatures#update"
+  delete "/creatures/:id", to: "creatures#destroy"
 end
