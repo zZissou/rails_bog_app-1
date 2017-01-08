8be87f96e21c8d4d175c363abc4a4264e0babc4a
zZissou <kyle.edward.russell@gmail.com>
2017-01-07 20:27:25 -0800

    completed part 3

--- real_bogg_app/app/controllers/creatures_controller.rb
+++ real_bogg_app/app/controllers/creatures_controller.rb
@@ -9,7 +9,6 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
 
     creature = Creature.new(creature_params)
 
@@ -22,5 +21,26 @@ class CreaturesController < ApplicationController
     creature_id = params[:id]
 
     @creature = Creature.find_by_id(creature_id)
+  end
+
+  def edit
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature.update_attributes(creature_params)
+
+    redirect_to creature_path(creature)
+  end
+
+  private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 end
--- real_bogg_app/config/routes.rb
+++ real_bogg_app/config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/creatures/new', to: 'creatures#new', as: 'new_creature'
   post '/creatures', to: 'creatures#create'
   get '/creatures/:id', to: 'creatures#show', as: 'creature'
+  get '/creatures/:id/edit', to: 'creatures#edit', as: 'edit_creature'
+  patch '/creatures/:id', to: 'creatures#update'
 end
