34311c54fd47228fda5b17431683df2c996ce56f
zZissou <kyle.edward.russell@gmail.com>
2017-01-07 20:32:09 -0800

    completed third run of bog app. Time = 37m 57s

--- real_bogg_app/app/controllers/creatures_controller.rb
+++ real_bogg_app/app/controllers/creatures_controller.rb
@@ -39,6 +39,16 @@ class CreaturesController < ApplicationController
     redirect_to creature_path(creature)
   end
 
+  def destroy
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature.destroy
+
+    redirect_to creatures_path
+  end
+
   private
   def creature_params
     params.require(:creature).permit(:name, :description)
