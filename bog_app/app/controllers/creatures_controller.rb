b564f56564273b3a4a9c4855d0693e7c4c6e74d1
zZissou <kyle.edward.russell@gmail.com>
2017-01-07 16:57:11 -0800

    make a creature with new and create

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -3,4 +3,24 @@ class CreaturesController < ApplicationController
   def index
     @creatures = Creature.all
   end
+
+  def new
+    @creatures = Creature.new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creatures_path(creature)
+    end
+  end
+
+  def show
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+  end
 end
